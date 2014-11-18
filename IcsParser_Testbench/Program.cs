bdfe82763c2291480aad1771b9ce2950e1a527eb
Elias Ruemmler <git@rc-art.de>
2014-11-18 16:41:50 +0100

    Beispiel "GetRunningEvents()" ergänzt

--- IcsParser_Testbench/Program.cs
+++ IcsParser_Testbench/Program.cs
@@ -13,38 +13,12 @@ namespace Rca.IcsParser
         static void Main(string[] args)
         {
             IcsParser myIcsParser = new IcsParser();
-            EventEntryList myEventList = myIcsParser.Parse(WebIcsFile);
-            //EventEntryList myEventList = myIcsParser.Parse(LocalIcsFile);
-            Console.WriteLine("Es wurden {0} Event-Einträge gefunden.", myEventList.Count);
-            EventEntry[] myEventEntryAry = myEventList.GetRecentEvents();
-            Console.WriteLine("Davon {0} Event-Einträge in der Vergangenheit...", myEventEntryAry.Length);
-            myEventEntryAry = myEventList.GetNextEvents();
-            Console.WriteLine("...und {0} Event-Einträge in für die Zukunft.", myEventEntryAry.Length);
-            Console.ReadKey();
-        }
-    }
-}
-using System;
-
-namespace Rca.IcsParser
-{
-    /// <summary>
-    /// Testbench für dei IcsParser-DLL
-    /// </summary>
-    class Program
-    {
-        static Uri WebIcsFile = new Uri(@"https://www.google.com/calendar/ical/ahhulle352l5hbn2uk6ojccrk8%40group.calendar.google.com/public/basic.ics");
-        static Uri LocalIcsFile = new Uri(new Uri(AppDomain.CurrentDomain.BaseDirectory), @"basic.ics");
-
-        static void Main(string[] args)
-        {
-            IcsParser myIcsParser = new IcsParser();
             EventEntryList myEventList = new EventEntryList();
 
             Boolean successSelected = false;
             while (!successSelected)
             {
-                Console.Write("\nAuswahl der ICS-Quelle: (W - Web, L - Lokal) ");
+                Console.Write("\nAuswahl der ICS-Quelle. (W: Web, L: Lokal) ");
 
                 switch (Console.ReadKey().Key)
                 {
@@ -68,6 +42,8 @@ namespace Rca.IcsParser
             Console.WriteLine("Davon {0} Event-Einträge in der Vergangenheit...", myEventEntryAry.Length);
             myEventEntryAry = myEventList.GetNextEvents();
             Console.WriteLine("...und {0} Event-Einträge in für die Zukunft.", myEventEntryAry.Length);
+            myEventEntryAry = myEventList.GetRunningEvents();
+            Console.WriteLine("{0} Event-Einträge fallen auf den heutigen Tag.", myEventEntryAry.Length);
             Console.ReadKey();
         }
     }
